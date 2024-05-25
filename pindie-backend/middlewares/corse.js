ecebce037d1f1c30dc36faa994292eda0eec7dbf
Pomidoroff-Komrad <149384618+Pomidoroff-Komrad@users.noreply.github.com>
2024-05-26 02:16:52 +0300

    add README files
    maybe fix cors

--- pindie-backend/middlewares/corse.js
+++ pindie-backend/middlewares/corse.js
@@ -1,11 +1,11 @@
 const allowedCors = [
+    'https://frontend-pindie-nzi.nomoredomainswork.ru',
+    'frontend-pindie-nzi.nomoredomainswork.ru',
     'http://localhost:3000/',
     'https://practicum.yandex.ru',
     'https://students-projects.ru',
     'http://localhost:3000',
-    'http://localhost:3001/',
-    'https://frontend-pindie-nzi.nomoredomainswork.ru'
- 
+    'http://localhost:3001/', 
   ];
   function corse(req, res, next) {
       const { origin } = req.headers; 
